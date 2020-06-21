46abd8177e5461299c6ae74cf066543ed4730f23
phsangit <startsboy@gmail.com>
2020-06-21 07:54:10 +0700

    Hoàng Sang
    Thêm Ảnh, thêm các mục cần thiết cho trang chủ

--- js/main.js
+++ js/main.js
@@ -8,4 +8,15 @@ $(".slider-one")
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next",
 });
-$
+
+function myLoader() {
+    alert("Chào các tình yêu, mua săm vui vẻ nhé! \nNhập mã HoàngSang để được giảm 90% nhé!");
+}
+
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+    });
+    $('#myCollapsible').collapse({
+        toggle: false
+    });
+    $('.carousel').carousel();
