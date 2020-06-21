ef20de0058a78d0ce342700643c8fb787496e4bd
phsangit <startsboy@gmail.com>
2020-06-21 22:40:53 +0700

    Hoàng Sang
    Xử lý giõ hàng

--- js/main.js
+++ js/main.js
@@ -8,15 +8,15 @@ $(".slider-one")
     prevArrow: ".site-slider .slider-btn .prev",
     nextArrow: ".site-slider .slider-btn .next",
 });
-
-function myLoader() {
-    alert("Chào các tình yêu, mua săm vui vẻ nhé! \nNhập mã HoàngSang để được giảm 90% nhé!");
-}
+// Khi cửa sổ được load sẽ tự gọi hàm alert ra
+$(window).on('load', function () {
+    alert("Chào các tình yêu, mua sắm vui vẻ nhé! \nNhập mã HoàngSang để được giảm 90% nhé!");
+});
 
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-    });
+});
     $('#myCollapsible').collapse({
         toggle: false
-    });
+});
     $('.carousel').carousel();
